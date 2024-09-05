161d38c77f9d93a9da04be2b7b34757189438411
ilyasse <ilyasseloubardi>
2024-09-05 14:07:19 +0100

    adding mevn project

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import project1 from "../assets/projects/Projet1.png";
 import project2 from "../assets/projects/Projet2.png";
 import project4 from "../assets/projects/LOGO.png";
+import project5 from "../assets/projects/mevnproject.png"
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. Although I am at the beginning of my journey, I am dedicated to mastering front-end technologies like React, Tailwind, and Bootstrap, as well as back-end technologies like Laravel and MySQL. My goal is to leverage my growing expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -14,6 +15,7 @@ export const PROJECTS = [
     description:
       "Resident Connect is a web app designed for managing residences efficiently.",
     technologies: ["tailwind", "React", "laravel", "Mysql"],
+    github:"https://github.com/ilyasse13/resident_connect"
   },
   {
     title: "Movie app front end",
@@ -21,6 +23,7 @@ export const PROJECTS = [
     description:
       "A front-end movie app that displays movies fetched from an API.",
     technologies: ["HTML", "CSS", "React"],
+    github:"https://github.com/ilyasse13/React_mv_app"
   },
  
   {
@@ -30,6 +33,13 @@ export const PROJECTS = [
       "A travel project highlighting restaurants, coffee shops, and nightclubs in Marseille, with reservation capabilities on the website.",
     technologies: ["HTML", "CSS", "tailwind", "React", "mySQL","Laravel"],
   },
+  {
+    title: "recipe sharing app",
+    image: project5,
+    description:"A recipe-sharing app that lets users post, browse, and explore a variety of dishes, from everyday meals to gourmet recipes. It fosters a community for cooking enthusiasts to share their creations and discover new ideas",
+    technologies: ["vue", "tailwind", "mongodb","express","nodejs"],
+    github:"https://github.com/ilyasse13/mevn_project"
+  }
 ];
 
 export const CONTACT = {
